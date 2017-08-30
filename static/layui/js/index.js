c5d5e9613242c6e9c3e5aa95528020b805e574f6
Kaiser <583075641@qq.com>
2017-08-30 23:03:37 +0800

    主页页头

--- static/layui/js/index.js
+++ static/layui/js/index.js
@@ -1,7 +1,23 @@
-layui.define(['element', 'form','common'], function(exports) {
-	var common = layui.common
-	, $ = layui.jquery
-	, form = layui.form
-	, element = layui.element;
+layui.define(['element', 'form', 'common'], function(exports) {
+	var common = layui.common,
+		form = layui.form,
+		element = layui.element;
+
+	common.setDate('#Date');
+	common.setTime('#Time');
+	setInterval(function() {
+		common.setDate('#Date')
+	}, 60000);
+	setInterval(function() {
+		common.setTime('#Time')
+	}, 1000);
+
+	$('.layui-unselect').addClass('layui-bg-transparent');
+	var nt_title = $('#nt-title').newsTicker({
+			row_height: 30,
+			max_rows: 1,
+			duration: 5000,
+			pauseOnHover: 0
+		});
 	exports('index', {});
 });
--- static/layui/js/model/common.js
+++ static/layui/js/model/common.js
@@ -3,7 +3,8 @@ layui.define(['layer'], function(exports) {
 	"use strict";
 
 	var $ = layui.jquery,
-		layer = layui.layer;
+		layer = layui.layer,
+		weekDay = ['星期日', '星期一', '星期二', '星期三', '星期四', '星期五', '星期六'];
 
 	var common = {
 		/**
@@ -22,6 +23,10 @@ layui.define(['layer'], function(exports) {
 				icon: 5
 			});
 		},
+		/**
+		 * 提示信息
+		 * @param {Object} msg
+		 */
 		msgSuccess: function(msg){
 			layer.msg(msg,{
 				icon:1
@@ -35,6 +40,22 @@ layui.define(['layer'], function(exports) {
             var reg = new RegExp("(^|&)" + key + "=([^&]*)(&|$)", "i");
             var r = window.location.search.substr(1).match(reg);
             if (r != null) return unescape(r[2]); return null;
+		},
+		/**
+		 * 设置年月日
+		 * @param {String} target
+		 */
+		setDate:function(target){
+			var newDate = new Date();
+			$(target).html(newDate.getFullYear() + '年' + (newDate.getMonth() + 1) + '月' + newDate.getDate() + '日  ' + weekDay[newDate.getDay()]);
+		},
+		/**
+		 * 设置时间
+		 * @param {Object} target
+		 */
+		setTime:function(target){
+			var newDate = new Date();
+			$(target).html(newDate.getHours()+':'+newDate.getMinutes()+':'+newDate.getSeconds()+' '+(newDate.getHours()<12?'AM':'PM'));
 		}
 	};
 	exports('common', common);
