341d68aafd583f9b3e2ad0de54210f272f6c867e
Kaiser <583075641@qq.com>
2017-08-31 22:15:17 +0800

    主页

--- static/layui/js/index.js
+++ static/layui/js/index.js
@@ -1,24 +1,102 @@
-layui.define(['element', 'form', 'common'], function(exports) {
+layui.define(['element', 'form', 'common', 'jqmenu'], function(exports) {
 	var common = layui.common,
 		form = layui.form,
-		element = layui.element;
+		element = layui.element,
+		menu = layui.jqmenu,
+		mainMenu = new menu();
+	jqIndex = function() {};
 
-	common.setDate('#Date');
-	common.setTime('#Time');
-	setInterval(function() {
-		common.setDate('#Date')
-	}, 60000);
-	setInterval(function() {
-		common.setTime('#Time')
-	}, 1000);
+	/**
+	 *初始化方法
+	 */
+	jqIndex.prototype.init = function() {
 
-	$('.layui-unselect').addClass('layui-bg-transparent');
-	var nt_title = $('#nt-title').newsTicker({
-		row_height: 30,
-		max_rows: 1,
-		duration: 5000,
-		pauseOnHover: 0
-	});
+		//      mainMenu.init('#menu-tpl',{icon:true,fresh:false});
+		this.showMenu();
+		this.showMenuMobile();
+		this.initDateAndTime();
+		this.initWeather();
+		this.initNewsTicker();
+		$('.layui-unselect').addClass('layui-bg-transparent');
+		
+	}
+	/**
+	 * 初始化轮播插件
+	 */
+	jqIndex.prototype.initNewsTicker = function(){
+		var nt_title = $('#nt-title').newsTicker({
+			row_height: 30,
+			max_rows: 1,
+			duration: 5000,
+			pauseOnHover: 0
+		});
+	}
+	/**
+	 * 初始化时间
+	 */
+	jqIndex.prototype.initDateAndTime = function(){
+		common.setDate('#Date');
+		common.setTime('#Time');
+		setInterval(function() {
+			common.setDate('#Date')
+		}, 60000);
+		setInterval(function() {
+			common.setTime('#Time')
+		}, 1000);
+	}
+	/**
+	 * 加载天气信息
+	 */
+	jqIndex.prototype.initWeather = function(){
+		tpwidget("init", {
+			"flavor": "slim",
+			"location": "WX4FBXXFKE4F",
+			"geolocation": "enabled",
+			"language": "auto",
+			"unit": "c",
+			"theme": "chameleon",
+			"container": "tp-weather-widget",
+			"bubble": "enabled",
+			"alarmType": "badge",
+			"color": "#FFFFFF",
+			"uid": "U41C5B006E",
+			"hash": "5f750df6e383777549478801d17f81fd"
+		});
+		tpwidget("show");
+	}
+	/**
+	 * 适配手机端
+	 */
+	jqIndex.prototype.showMenuMobile = function() {
+		//手机设备的简单适配
+		var treeMobile = $('.site-tree-mobile'),
+			shadeMobile = $('.site-mobile-shade')
 
+		treeMobile.on('click', function() {
+			$('body').addClass('site-mobile');
+			$('#menuDiv').removeClass('layui-bg-transparent').addClass('layui-bg-black');
+		});
+
+		shadeMobile.on('click', function() {
+			$('body').removeClass('site-mobile');
+		});
+	}
+	/**
+	 *绑定左侧菜单显示隐藏按钮单击事件
+	 */
+	jqIndex.prototype.showMenu = function() {
+		$('.hideMenu').bind("click", function() {
+			var locationShowType = layui.data('showType');
+			var showType = locationShowType.moveType ? locationShowType.moveType == 1 ? 2 : 1 : 2;
+			layui.data('showType', {
+				key: 'moveType',
+				value: showType
+			});
+			console.log(showType);
+			//          mainMenu.menuShowType();
+		})
+	}
+	var index = new jqIndex();
+	index.init();
 	exports('index', {});
 });
